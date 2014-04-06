1e70c9a6780f483c96796d5643da3ab7988eec3a
Dale Hamel <dale.hamel@srvthe.net>
2014-04-06 00:33:35 -0400

    Updating sidebar

--- doc/titler.sh
+++ doc/titler.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-for each in *.markdown; do 
-  date=` echo $each | sed 's/\(..........\).*/\1/g'`
-
-#modified: 2013-05-31
-  sed  -i "s/\(^title.*$\)/\1\nmodified: $date/g" $each
+for each in _posts/*.markdown; do 
+  #name=` echo $each | sed 's/...........\(.*\).markdown/\1/g' | tr -s "-" " "  | sed -e "s/\b\(.\)/\u\1/g"`;  
+  
+  
+  grep wordpress_url: doc/convert/todo/$each
+  sed -i "s/ title/ $name/g" $each
 done  
 
